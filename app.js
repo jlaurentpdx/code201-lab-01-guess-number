fbfaa0239fbcee59bd29bd968bc0b5e7f2fc0f25
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-04 15:00:59 -0700

    created HTML layout

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// import functions and grab DOM elements
-
-// initialize global state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// get DOM elements
+// set a variable to hold a random number between 1 and 20 using Math.Random()
+// create a button event listener with an if... else statement:
+//  if userGuess === randomNum, display "you win!"
+//  else if guessesRemaining === 0, display "you're out of guesses"
+//  else if userGuess > 20 or < 1, display "invalid number"
+//  else if userGuess > randomNum, display "Too high"
+//  else if userGuess < randomNum, display "Too low"
+//  else break
+// 
