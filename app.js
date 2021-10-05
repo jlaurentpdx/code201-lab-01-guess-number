999c1c2c515cbf90875ffeb7c27889ec6d2edc38
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-04 17:28:32 -0700

    fixed if/else checks, verified working win/lose/play states

--- app.js
+++ app.js
@@ -1,45 +1,32 @@
-// get DOM elements
-// let randomNum = a random number between 1 and 20 using Math.Random()
-// create a button event listener with an if... else statement:
-//      if userGuess === randomNum, display "you win!"
-//      else if guessesRemaining === 0, display "you're out of guesses"
-//      else if userGuess > randomNum, display "Too high", decrement guesses
-//      else if userGuess < randomNum, display "Too low", decrement guess
-//      else display "invalid guess"
-// 
-
-const userGuess = document.getElementById('user-guess');
+const userInput = document.getElementById('user-input');
 const guessButton = document.getElementById('guess-button');
 const guessResult = document.getElementById('guess-result');
 const guessesRemaining = document.getElementById('guesses-remaining');
 
-const prevGuesses = [];
-
+let prevGuesses = [];
 let randomNum = Math.ceil(Math.random() * 20);
 let numGuesses = 4;
 
+console.log(randomNum);
+
 guessButton.addEventListener('click', () => {
-    // reduce the number of guesses remaining
+    let userGuess = Number(userInput.value);
     numGuesses--;
-    // calculate and show the user the number of guesses left
     guessesRemaining.textContent = numGuesses;
 
-    // check the user's most recent guess (userGuess)
-    // if userGuess === randomNum, guessResult = "you win!"
-    //  else if guessesRemaining === 0, guessResult = "you're out of guesses"
-    //  else if userGuess > randomNum, guessResult = "Too high", decrement guesses
-    //  else if userGuess < randomNum, display "Too low", decrement guess
-    //  else display "invalid guess"
+    console.log(userGuess);
 
-    if(userGuess === randomNum) {
+    if (userGuess === randomNum) {
         guessResult.textContent = 'You got it!'; 
-    } else if(guessesRemaining === 0){
+    } else if (numGuesses <= 0){
         guessResult.textContent = 'You\'re out of guesses!';
-    } else if(userGuess > randomNum) {
-        guessResult.textContent = 'Too high...'
-    } else if(userGuess < randomNum) {
-        guessResult.textContent = 'Too low...'
+    } else if (userGuess > randomNum) {
+        guessResult.textContent = 'Too high...';
+    } else if (userGuess < randomNum) {
+        guessResult.textContent = 'Too low...';
     } else {
-        guessResult.textContent = 'What have you done...?'
+        guessResult.textContent = 'What have you done...?';
     }
-})
+
+    prevGuesses.push(userGuess);
+});
